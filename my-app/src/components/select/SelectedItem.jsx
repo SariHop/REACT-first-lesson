aaffd796db0b5dac339e301f3ef6cedd6645172f
SariHop <sarihop12@gmail.com>
2024-10-25 15:40:47 +0300

    useEffect console.log

--- my-app/src/components/select/SelectedItem.jsx
+++ my-app/src/components/select/SelectedItem.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const SelectedItem = ({ item_state }) => {
+
+    useEffect(() => {
+        console.log(item_state)
+    }, [item_state])
+
     return <div> {`Selected: ${item_state}`}</div>;
 };
 
