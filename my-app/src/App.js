4b88dce7544eb2be648ba7169994a8f60a253c25
SariHop <sarihop12@gmail.com>
2024-10-25 14:43:27 +0300

    create react app

---
+++ my-app/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import Paragraph from './components/Paragraph';
+import Link from './components/Link';
+import Footer from './components/Footer';
+import Input from './components/Input';
+
+function App() {
+
+  const fun = ()=>{
+    return "functhion return"
+  }
+
+  return (
+    <div className="App">
+     
+    </div>
+  );
+}
+
+export default App;
+
