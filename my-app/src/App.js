cd026bbb40d7a3ccf4e2e1b12f4f9b67ed7272ab
SariHop <sarihop12@gmail.com>
2024-10-27 13:15:12 +0200

    router submit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,19 @@
 import './App.css';
 import Header from './components/5_components/Header';
 import Paragraph from './components/5_components/Paragraph';
-import Link from './components/5_components/Link';
+// import Link from './components/5_components/Link';
 import Footer from './components/5_components/Footer';
 import Input from './components/5_components/Input';
 import ItemList from './components/select/ItemList';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
+
+  const [name, setName] = useState('Sari')
+  useEffect(() => {
+    console.log("Updated name in App:", name);
+  }, [name]);
 
   // const fun = ()=>{
   //   return "functhion return"
@@ -21,8 +28,24 @@ function App() {
       <Input></Input>
       <Footer></Footer> */}
 
-      <ItemList></ItemList>
-      
+      {/* <ItemList></ItemList> */}
+
+      <BrowserRouter>
+        <Link to='/Header'>Header</Link>
+        <br />
+        <Link to='/Input'>Input</Link>
+        <br />
+        <Link to='/Footer'>Footer</Link>
+        <br />
+        <br />
+
+        <Routes>
+          <Route path="Header" element={<Header name={name}></Header>} />
+          <Route path="Input" element={<Input setName={setName}></Input>} />
+          <Route path="Footer" element={<Footer></Footer>} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
--- my-app/src/components/5_components/Input.jsx
+++ my-app/src/components/5_components/Input.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Input = () => {
-  return <div>Input</div>;
+const Input = ({ setName }) => {
+  const [inputValue, SetinputValue] = useState('')
+
+  const click = () => {
+    console.log("Input value:", inputValue);
+    setName(inputValue)
+  }
+
+  return <>
+    <input onChange={(e) => { SetinputValue(e.target.value) }}></input>
+    <button onClick={click}>button</button>
+  </>;
 };
 
 export default Input;
